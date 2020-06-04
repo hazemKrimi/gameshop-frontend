0904a92e20828b625400aa2d096f4954432c9984
Hazem Krimi <krimihazem1@gmail.com>
2020-06-04 12:25:17 +0100

    Added authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,47 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { MainContext } from '../contexts/MainContext';
 import { Jumbotron, Form, FormGroup, Input, Button, Row, Col } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 const Login = () => {
-    return (
+    const { user, logIn } = useContext(MainContext);
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const log = async() => {
+        try {
+            await logIn(email, password);
+        } catch(err) {
+            alert(err.msg ? err.msg : 'Cannot log in, please check your credentials or try again later');
+        }
+    };
+
+    return !user ? (
         <Jumbotron className="container">
-            <Form>
+            <Form onSubmit={event => { event.preventDefault(); log(); }}>
                 <h2>Login</h2>
                 <FormGroup>
-                    <Input type="email" name="email" placeholder="Email" required />
+                    <Input type="email" name="email" placeholder="Email" required value={email} onChange={event => setEmail(event.target.value)} />
                 </FormGroup>
                 <FormGroup>
-                    <Input type="password" name="password" placeholder="Password" required />
+                    <Input type="password" name="password" placeholder="Password" required value={password} onChange={event => setPassword(event.target.value)} />
                 </FormGroup>
                 <FormGroup>
                     <Row>
                         <Col xs="6">
-                            <Button>Login</Button>
+                            <Button type='submit'>Login</Button>
                         </Col>
                         <Col xs="6">
                             <p style={{ textAlign: "right" }}>
-                                Don't have an Account? <Link style={{ color: "#007bff"}} to="/signup">Sign Up</Link>
+                                Don't have an Account? <Link style={{ color: "#007bff" }} to="/signup">Sign Up</Link>
                             </p>
                         </Col>
                     </Row>
                 </FormGroup>
             </Form>
         </Jumbotron>
+    ) : (
+        <Redirect to='/home' />
     );
 };
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { MainContext } from '../contexts/MainContext';
 import { Jumbotron, Form, FormGroup, Input, Button } from 'reactstrap';
+import { Redirect } from 'react-router-dom';
 
 const SignUp = () => {
-    return (
+    const { user, signUp } = useContext(MainContext);
+    const [ name, setName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const sign = async() => {
+        try {
+            await signUp(name, email, password);
+        } catch(err) {
+            alert(err.msg ? err.msg : 'Cannot sign up currently, please try again later');
+        }
+    };
+
+    return !user ? (
         <Jumbotron className="container">
-            <Form>
+            <Form onSubmit={event => { event.preventDefault(); sign(); }}>
                 <h2>Sign Up</h2>
                 <FormGroup>
-                    <Input type="text" name="username" placeholder="Username" required />
+                    <Input type="text" placeholder="Username" required value={name} onChange={event => setName(event.target.value)} />
                 </FormGroup>
                 <FormGroup>
-                    <Input type="email" name="email" placeholder="Email" required />
+                    <Input type="email" placeholder="Email" required value={email} onChange={event => setEmail(event.target.value)} />
                 </FormGroup>
                 <FormGroup>
-                    <Input type="password" name="password" placeholder="Password" required />
+                    <Input type="password" placeholder="Password" required value={password} onChange={event => setPassword(event.target.value)} />
                 </FormGroup>
                 <FormGroup>
-                    <Button>Sign Up</Button>
+                    <Button type='submit'>Sign Up</Button>
                 </FormGroup>
             </Form>
         </Jumbotron>
+    ) : (
+        <Redirect to='/home' />
     );
 };
 
