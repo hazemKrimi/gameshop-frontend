88303a2ca3e292e287eb2d919d8cb6cb2aef0262
Hazem Krimi <krimihazem1@gmail.com>
2020-06-04 12:27:31 +0100

    Added users logic

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MainContext } from '../contexts/MainContext';
 import { Jumbotron, Button } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
-const LandingPage = () => (
-    <Jumbotron className="container text-center">
-        <h2>Welcome to the place where Gamers and Game Developers meet</h2>
-        <p>GameShop is a platform where Gamers and Game Developers share their games</p>
-        <div className="mt-2">
-            <Button className="btn-block m-auto" style={{width: "50%", fontWeight: "bold"}}>
-                <Link to="/signup">Get Started</Link>
-            </Button>
-        </div>
-    </Jumbotron>
-);
+const LandingPage = () => {
+    const { user } = useContext(MainContext);
+
+    return !user ? (
+        <Jumbotron className="container text-center">
+            <h2>Welcome to the place where Gamers and Game Developers meet</h2>
+            <p>GameShop is a platform where Gamers and Game Developers share their games</p>
+            <div className="mt-2">
+                <Button className="btn-block m-auto" style={{ width: "50%", fontWeight: "bold" }}>
+                    <Link to="/signup">Get Started</Link>
+                </Button>
+            </div>
+        </Jumbotron>
+    ) : (
+        <Redirect to='/home' />
+    );
+};
 
 export default LandingPage;
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,33 +1,58 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { MainContext } from '../contexts/MainContext';
 import { Jumbotron, Form, FormGroup, Input, Button } from 'reactstrap';
+import { Redirect } from 'react-router-dom';
 
 const Settings = () => {
-    return (
+    const { user, updateUser, deleteUser } = useContext(MainContext);
+    const [ name, setName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const update = async () => {
+        try {
+            await updateUser(name, email, password);
+        } catch (err) {
+            alert(err.msg ? err.msg : 'Cannot update currently, please try again later');
+        }
+    };
+
+    const del = async() => {
+        try {
+            await deleteUser();
+        } catch (err) {
+            alert(err.msg ? err.msg : 'Cannot delete currently, Please try again later');
+        }
+    };
+
+    return user ? (
         <Jumbotron className="container">
-            <h1>Account Settings for <b>Hik0o</b>:</h1>
-            <Form>
+            <h1>Account Settings for <b>{user.name}</b>:</h1>
+            <Form onSubmit={event => { event.preventDefault(); update(); }}>
                 <h2>Update Account</h2>
                 <p>To change a credential enter a new one, else leave it empty (Always fill the username for Confirmation)</p>
                 <FormGroup>
-                    <Input type="text" name="username" placeholder="New Username" required />
+                    <Input type="text" name="username" placeholder="New Username" required value={name} onChange={event => setName(event.target.value)} />
                 </FormGroup>
                 <FormGroup>
-                    <Input type="email" name="email" placeholder="New Email" required />
+                    <Input type="email" name="email" placeholder="New Email" required value={email} onChange={event => setEmail(event.target.value)} />
                 </FormGroup>
                 <FormGroup>
-                    <Input type="password" name="password" placeholder="New Password" required />
+                    <Input type="password" name="password" placeholder="New Password" required value={password} onChange={event => setPassword(event.target.value)} />
                 </FormGroup>
                 <FormGroup>
-                    <Button>Update</Button>
+                    <Button type='submit'>Update</Button>
                 </FormGroup>
             </Form>
             <Form>
                 <h2>Delete Account</h2>
                 <p>If you proceed you cannot recover your account</p>
-                <Button>Delete</Button>
+                <Button color='danger' onClick={event => { event.preventDefault(); del(); } }>Delete</Button>
             </Form>
         </Jumbotron>
-    );
+    ) : (
+        <Redirect to='/login' />
+    )
 };
 
 export default Settings;
